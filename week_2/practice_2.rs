9db073d6da4240dfd4d7391d9fb4919c888951f2
Your Name <you@example.com>
2025-10-06 19:55:29 +0100

    Prints user name and some texts

---
+++ week_2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main(){
+    println!();
+
+    println!("hello ");
+    println!("format {} arguments", "some");
+
+    println!("My name is {}. I am the rep of stream", "Dagogo William-Jumbo");
+
+
+}
