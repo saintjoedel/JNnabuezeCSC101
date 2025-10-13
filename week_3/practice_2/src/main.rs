db305f3265ee116789da1a6087171fe70a4c1c76
Your Name <you@example.com>
2025-10-13 21:05:22 +0100

    using variables to performe operations

---
+++ week_3/practice_2/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+let result = 10; // 132 by default
+let age:u32 = 20;
+let sum = 132-515;
+println! ("Result value is {} ", result);
+println! ("Age is {} ",age);
+println! ("Sum is {} ",sum);
+}
